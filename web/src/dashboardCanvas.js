f873366e3a705c6284d4840b1bc02cff4b986390
StevenRCE0 <wangyanbo02@me.com>
2020-12-15 21:40:45 +0800

    Extended dependency

--- web/src/dashboardCanvas.js
+++ web/src/dashboardCanvas.js
@@ -1,15 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Rect} from "react-konva";
+import React from 'react'
+import Portal from "./Portal";
+import {Layer, Rect, Stage} from "react-konva";
+import {CircularProgress} from "@material-ui/core";
 
 class Dashboard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {size: 300, rounded: 20, circle0: '#A00'}
+    }
     render() {
         return (
-            <Rect
-                x={10} y={10} width={50} height={50}
-                fill={this.state.color}
-                shadowBlur={10}
-            />
+            <Stage width={this.state.size} height={this.state.size}>
+                <Layer>
+                    <Rect
+                        x={10} y={10} width={this.state.size - 20} height={this.state.size - 20}
+                        fill={'#EEE'}
+                        shadowBlur={10}
+                        cornerRadius={this.state.rounded}
+                    />
+                </Layer>
+                <Layer>
+                    <Portal>
+                        <CircularProgress
+                            style={{
+                                position: 'absolute',
+                                
+                                top: this.state.rounded,
+                                left: this.state.rounded,
+                            }}
+                            variant={'determinate'}
+                            value={70}
+                        />
+                    </Portal>
+                </Layer>
+            </Stage>
         )
     }
 }
