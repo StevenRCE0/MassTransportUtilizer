246aa7f2456a6fafd5511915c6d174b06d9a8e0b
Tonal-all <76808569+Tonal-all@users.noreply.github.com>
2021-01-25 20:56:05 +0800

    null
    null

--- java/demo01/com/basic/Cope.java
+++ java/demo01/com/basic/Cope.java
@@ -16,7 +16,7 @@ public class Cope {
         try {
             Class.forName("com.mysql.jdbc.Driver");
             long start = System.currentTimeMillis();
-            conn = DriverManager.getConnection("jdbc:mysql://124.70.94.200:3306/library1", "root", "Maker0000");
+            conn = DriverManager.getConnection("jdbc:mysql://124.70.94.200:3306/library1", "root", "Maker0000");// 最近会换数据库2333
             long end = System.currentTimeMillis();
             System.out.println(conn);
             System.out.println("建立连接耗时：  " + (end - start) + "ms  毫秒");
