c22ed9bf10c727e570f99a2ba17287fcbe4f0fc1
StevenRCE0 <wangyanbo02@me.com>
2021-02-14 14:34:20 +0800

    Keyboard shortcuts

--- web/src/App.js
+++ web/src/App.js
@@ -30,15 +30,29 @@ function App() {
 function AnimationApp() {
     const location = useLocation();
     const modalElement = []
+
+    window.addEventListener("keydown", function (e) {
+        if (e.defaultPrevented) {
+            return;
+        }
+        if (e.key !== undefined) {
+            if (e.key === '1') {window.location.replace('/Overview')}
+            if (e.key === '2') {window.location.replace('./PassengerAnalytics')}
+        }
+        else if (e.code !== undefined) {
+            if (e.code === 'Digit1') {window.location.replace('/Overview')}
+            if (e.code === 'Digit2') {window.location.replace('/PassengerAnalytics')}
+        }
+    })
     return (
         <React.Fragment>
             {modalElement}
             <div className="Dock">
                 <NavLink to={"Overview"} activeClassName={"active"} exact>
-                    <button className={"DockNavigation"}>概览</button>
+                    <button className={"DockNavigation"}>概览<span>1</span></button>
                 </NavLink>
                 <NavLink to={"PassengerAnalytics"} activeClassName={"active"}>
-                    <button className="DockNavigation">客流情况分析</button>
+                    <button className="DockNavigation">客流情况分析<span>2</span></button>
                 </NavLink>
             </div>
             <TransitionGroup>
