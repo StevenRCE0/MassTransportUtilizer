88daef57db035bf65091169ef4554481a43d1502
StevenRCE0 <wangyanbo02@me.com>
2021-02-23 12:35:16 +0800

    Dirtified routers

--- web/src/App.js
+++ web/src/App.js
@@ -12,66 +12,65 @@ import Overview from "./Overview";
 import PassengerAnalytics from "./PassengerAnalytics";
 import './index.css';
 
-function App() {
-    return (
-        <Router>
-            <Switch>
-                <Route exact path={"./"}>
-                    <Redirect to={"Overview"}/>
-                </Route>
-                <Route path={"*"}>
-                    <AnimationApp/>
-                </Route>
-            </Switch>
-        </Router>
-    );
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path={"./"}>
+                        <Redirect to={"Overview"}/>
+                    </Route>
+                    <Route path={"*"} component={AnimationApp}/>
+                </Switch>
+            </Router>
+        )
+    }
 }
 
-function AnimationApp() {
-    const location = useLocation();
-    const { match } = this.props;
-
-    window.addEventListener("keydown", function (e) {
-        if (e.defaultPrevented) {
-            return;
-        }
-        function handleShortcutKey(key) {
-            const destination = ['Overview', 'PassengerAnalytics']
-            window.location.replace('./' + destination[key - 1])
-        }
-        if (e.key !== undefined) {
-            if (e.key === '1') {handleShortcutKey(1)}
-            if (e.key === '2') {handleShortcutKey(2)}
-        }
-        else if (e.code !== undefined) {
-            if (e.code === 'Digit1') {handleShortcutKey(1)}
-            if (e.code === 'Digit2') {handleShortcutKey(2)}
-        }
-    })
-    return (
-        <React.Fragment>
-            <div className="Dock">
-                <NavLink to={"Overview"} activeClassName={"active"} exact>
-                    <button className={"DockNavigation"}>概览<span>1</span></button>
-                </NavLink>
-                <NavLink to={"PassengerAnalytics"} activeClassName={"active"}>
-                    <button className="DockNavigation">客流情况分析<span>2</span></button>
-                </NavLink>
-            </div>
-            <TransitionGroup>
-                <CSSTransition
-                    key={location}
-                    classNames={"fade"}
-                    timeout={250}
-                >
-                    <Switch>
-                        <Route path={`${match.url}/Overview`} component={Overview}/>
-                        <Route path={`${match.url}/PassengerAnalytics`} component={PassengerAnalytics}/>
-                    </Switch>
-                </CSSTransition>
-            </TransitionGroup>
-        </React.Fragment>
-    )
+class AnimationApp extends React.Component {
+    render() {
+        window.addEventListener("keydown", function (e) {
+            if (e.defaultPrevented) {
+                return;
+            }
+            function handleShortcutKey(key) {
+                const destination = ['Overview', 'PassengerAnalytics']
+                window.location.replace('./' + destination[key - 1])
+            }
+            if (e.key !== undefined) {
+                if (e.key === '1') {handleShortcutKey(1)}
+                if (e.key === '2') {handleShortcutKey(2)}
+            }
+            else if (e.code !== undefined) {
+                if (e.code === 'Digit1') {handleShortcutKey(1)}
+                if (e.code === 'Digit2') {handleShortcutKey(2)}
+            }
+        })
+        return (
+            <React.Fragment>
+                <div className="Dock">
+                    <NavLink to={"Overview"} activeClassName={"active"} exact>
+                        <button className={"DockNavigation"}>概览<span>1</span></button>
+                    </NavLink>
+                    <NavLink to={"PassengerAnalytics"} activeClassName={"active"}>
+                        <button className="DockNavigation">客流情况分析<span>2</span></button>
+                    </NavLink>
+                </div>
+                <TransitionGroup>
+                    <CSSTransition
+                        key={this.props.location}
+                        classNames={"fade"}
+                        timeout={250}
+                    >
+                        <Switch>
+                            <Route path={`*/Overview`} component={Overview}/>
+                            <Route path={`*/PassengerAnalytics`} component={PassengerAnalytics}/>
+                        </Switch>
+                    </CSSTransition>
+                </TransitionGroup>
+            </React.Fragment>
+        )
+    }
 }
 
 export default App;
